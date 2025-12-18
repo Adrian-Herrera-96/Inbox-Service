c5ed823bb7eef6e1076d1c10abf34de5d541910c
gongor7 <crisgongor7@gmail.com>
2025-12-18 13:30:52 -0400

    Creación de modulo workflow

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
 import { InboxModule } from './inbox/inbox.module';
+import { WorkflowsModule } from './workflows/workflows.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, CommonModule, InboxModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    CommonModule,
+    InboxModule,
+    WorkflowsModule,
+  ],
   controllers: [],
   providers: [],
 })
