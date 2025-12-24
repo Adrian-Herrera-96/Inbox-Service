992481a09458040166dc0a7063ed1e1b632da5a4
gongor7 <crisgongor7@gmail.com>
2025-12-24 12:04:08 -0400

    cambio de nombre de dto

--- src/inbox/dtos/find-procedure-current-area.dto.ts
+++ src/inbox/dtos/find-procedure-current-area.dto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
-export class FindProcedureStateDto {
+export class FindProcedureCurrentAreaDto {
   @IsNumber()
   @IsNotEmpty()
   typeId: number;
